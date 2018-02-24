c1abd2818604539b243d459a4b972a6285fedc1b
Noel Mendoza <nmendozavd@gmail.com>
2018-02-23 17:39:55 -0800

    initial commmit

---
+++ WhileYouWait.js
@@ -0,0 +1,10 @@
+var daysUntilMyBirthday = 31
+if (daysUntilMyBirthday > 30) {
+  console.log(daysUntilMyBirthday, 'days until my birthday. sad sad days...')
+}else if (daysUntilMyBirthday <= 30 > 1) {
+  console.log(daysUntilMyBirthday, 'Lets gooooo!!!')
+}else {
+  console.log(` ♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*
+				♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪
+				*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«`)
+}
